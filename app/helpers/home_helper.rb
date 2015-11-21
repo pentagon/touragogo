d251ac053fc5968a88719def845ce7e43bf21ba6
Viacheslav Petrenko <slavap@gmail.com>
2015-11-21 19:48:23 +0200

    Guide seed

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -10,4 +10,8 @@ module HomeHelper
   def language_opts_for_select
     Tour.distinct(:language).map { |c| [c, c] }
   end
+
+  def current_user_tourist?
+    current_user && current_user.role == 2
+  end
 end
