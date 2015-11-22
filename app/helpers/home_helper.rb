2c86ee7f77156e655bf0bdf8e4e5e68fe52c1812
Alexander Chepurnoy <chepurnoy.aleksandr@gmail.com>
2015-11-22 15:54:11 +0200

    Add countries and languages list.

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,14 +1,20 @@
 module HomeHelper
   def country_opts_for_select
-    Tour.distinct(:country).map { |c| [c, c] }
+    Tour.distinct(:country).map do |key|
+      label = ISO3166::Country.new(key).to_s.presence || key
+      [key, label]
+    end
   end
 
   def city_opts_for_select
-    Tour.distinct(:city).map { |c| [c, c] }
+    Tour.distinct(:city).map { |key| [key, key] }
   end
 
   def language_opts_for_select
-    Tour.distinct(:language).map { |c| [c, c] }
+    Tour.distinct(:language).map do |key|
+      label = ISO_639::find(key).try(:english_name) || key
+      [key, label]
+    end
   end
 
   def current_user_tourist?
