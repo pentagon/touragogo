31ad8042de7c81724fdee76df04d3e492c8cfd57
Alexander Chepurnoy <chepurnoy.aleksandr@gmail.com>
2015-11-22 14:03:11 +0200

    Allow setting marker for tour location.

--- app/models/tour.rb
+++ app/models/tour.rb
@@ -8,6 +8,8 @@ class Tour
   field :city, type: String
   field :language, type: String
   field :date, type: Time
+  field :latitude, type: Float
+  field :longitude, type: Float
 
   belongs_to :owner, class_name: 'User'
 
